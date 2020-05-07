8078ad3339459333e0774fe30c5681f19321e925
MichaelChan <MichaelChan90@outlook.com>
2020-05-07 14:17:59 +0800

    删除脚本

--- bin/git-push.bat
+++
@@ -1,3 +0,0 @@
-SET path=%~dp0
-SET git_path=%1%
-start %git_path% --cd="%path%.." -c "git add . ^& git commit -m '推送文档' ^& git push ^& hexo clean ^& hexo g  ^& hexo g "
--- bin/hexo-local.bat
+++
@@ -1,5 +0,0 @@
-SET path=%~dp0
-cd /d %path%
-cd ..
-@echo off
-hexo clean & hexo g & hexo s
